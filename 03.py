b4ad423029763073672801f7b7163fdfb1476c6a
Judith <judith@jmvdw.nl>
2025-12-03 20:14:19 +0100

    clean-up

--- 03.py
+++ 03.py
@@ -5,18 +5,15 @@ def joltage(bank, n):
         needed = n-len(jolts)
         if needed == 1:
             num = max(bank)
-        else: # don't consider last places, because we still need more numbers after
+        else:  # don't consider last places, because we still need more numbers after
             num = max(bank[:-needed+1])
         jolts.append(num)
         loc = bank.index(num)
         bank = bank[loc+1:]
-    ret = int("".join(map(str, jolts)))
-    return ret
+    return int(''.join(map(str, jolts)))
 
 
 with open('inputs/d3.txt') as f:
     banks = f.read().splitlines()
     print(sum(joltage(bank, n=2) for bank in banks))
     print(sum(joltage(bank, n=12) for bank in banks))
-
-
