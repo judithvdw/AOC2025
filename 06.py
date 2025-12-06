aa645de7d293bd757c09b87fada9117b2d8836fa
Judith <judith@jmvdw.nl>
2025-12-06 13:01:33 +0100

    day 6

--- 06.py
+++ 06.py
@@ -1,47 +1,44 @@
 from math import prod
 
 
-def calc(s):
-    ns = s[:-1]
-    operator = s[-1]
+def calc(s, operator):
     if operator == '+':
-        return sum(ns)
+        return sum(s)
     else:
-        return prod(ns)
+        return prod(s)
 
 
-with open('inputs/test.txt') as f:
+def parse_spaces(rows):
+    cuts = set()
+    for i, char in enumerate(rows[-1][1:]):  # loop over the operators, except the first to find the cuts
+        if char != ' ':
+            cuts.add(i)
+
+    split_rows = []
+    for row in rows[:-1]:  # don't need to do this for the operators
+        cur_row = []
+        buffer = ''
+        for i, char in enumerate(row):
+            if i in cuts:
+                cur_row.append(buffer)
+                buffer = ''
+            else:
+                buffer += char
+        cur_row.append(buffer)
+        split_rows.append(cur_row)
+    return split_rows
+
+
+with open('inputs/d6.txt') as f:  # added an extra column of 0's and a '+' to make it work
     rows = f.read().splitlines()
 
-num_rows = [list(map(int, row.split())) for row in rows[:-1]]
+number_rows = [list(map(int, row.split())) for row in rows[:-1]]
 operators = rows[-1].split()
-sums = list(zip(*(num_rows + [operators])))
-print(sums)
 
-print(sum(calc(s) for s in sums))
-#
-# cuts = set()
-# for i, char in enumerate(rows[-1][1:]):  # loop over the operators, except the first to find the cuts
-#     if char != ' ':
-#         cuts.add(i)
-#
-# split_rows = []
-# for row in rows[:-1]: # don't need to do this for the operators
-#     cur_row = []
-#     buffer = ''
-#     for i, char in enumerate(row):
-#         if i in cuts:
-#             cur_row.append(buffer)
-#             buffer = ''
-#         else:
-#             buffer += char
-#     cur_row.append(buffer)
-#     split_rows.append(cur_row)
-#
-# to_calc = list(zip(*split_rows))
-# pivoted = [list(map(lambda t: int("".join(t)), zip(*n))) for n in to_calc]
-#
-# print(pivoted)
-#
-#
-#
+sums = list(zip(*number_rows))  # pivot the rows/cols so the right groups end up together
+print(sum(calc(s, op) for s, op in zip(sums, operators)))
+
+
+sums = list(zip(*parse_spaces(rows)))
+pivoted = [list(map(lambda x: int(''.join(x)), zip(*s))) for s in sums]
+print(sum(calc(s, op) for s, op in zip(pivoted, operators)))
