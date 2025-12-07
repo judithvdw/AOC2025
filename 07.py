f12eb68fc511ee9fc2b04327e90437c1c10e1e4e
Judith <judith@jmvdw.nl>
2025-12-07 22:35:33 +0100

    day 7

--- 07.py
+++ 07.py
@@ -1,23 +1,28 @@
+from collections import defaultdict
+
 from utils.parsing import parse_grid
 
 
-def run_simulation(start, splitters, h=16, w=16):
-    active_beams = start
+def track_routes(start, splitters, h=142, w=142):
+    active_beams = defaultdict(int)
+    active_beams[start.pop()] = 1
     splits = 0
     for y in range(1, h):
         for x in range(w):
             if (x, y) in splitters and (x, y - 1) in active_beams:
                 splits += 1
-                active_beams.update({(x - 1, y), (x + 1, y)})
-                active_beams.remove((x, y - 1))
+                active_beams[(x - 1, y)] += active_beams[(x, y - 1)]
+                active_beams[(x + 1, y)] += active_beams[(x, y - 1)]
+                del active_beams[(x, y - 1)]
             if (x, y - 1) in active_beams:
-                active_beams.add((x, y))
-    return splits
+                active_beams[(x, y)] += active_beams[(x, y - 1)]
+                del active_beams[(x, y - 1)]
+    return splits, sum(active_beams.values())
 
 
 with open('inputs/d7.txt') as f:
     grid = parse_grid(f.read().splitlines())
-    splitters = grid['^']
-    start = grid['S']
 
-print(run_simulation(start, splitters, 142, 142))
+p1, p2 = track_routes(grid['S'], grid['^'])
+print(p1)
+print(p2)
